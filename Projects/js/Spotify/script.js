a2ac8bdfc036c96d2c64c879cc42877b755ae05a
Rustam Singh Tomer <rustamstomer@gmail.com>
2026-02-16 23:54:48 +0530

    adding functionality to fetch songs

--- Projects/js/Spotify/script.js
+++ Projects/js/Spotify/script.js
@@ -1,1 +1,23 @@
-console.log("Let's write js")
+
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/Spotify/songs/");
+    let response = await a.text();
+    // console.log(response);
+    let div = document.createElement('div');
+    div.innerHTML = response;
+    let as = div.getElementsByTagName('a');
+    let songs = [];
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith('.mp3')) {
+            songs.push(element.href);
+        }
+    }
+    return songs;
+}
+
+function main() {
+    let songs = getSongs();
+    console.log(songs);
+}
+
